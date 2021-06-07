cc14ff8cb8ba2c365de6e7ac38f09dd2b29c577f
BVaishnavChandra <74224476+BVaishnavChandra@users.noreply.github.com>
2021-06-07 18:46:59 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+// add code for displaying rest of the names and the hints.
+        
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170);
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,280)
+    fill("lightblue")
+    text("Hint: stores all information !!", 100,300);
+
+    textSize(20)
+    fill("yellow")
+    text("follow the hints and find the answers for the riddles", 540,420);
+    text("your target is to complete the riddles and unlock the treasury", 360,380);
+    text("If you answers the riddle correctly, the scoreboard increases up by 1", 320,460);
+}
